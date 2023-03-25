47aafa31e35f2894dea6dc04caef7bc1fa3c9b6b
Captain-ss <swadeshishu2001@gmail.com>
2023-03-25 15:16:06 +0530

    first commit

---
+++ client/src/Component/SignIn.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/SignInImg/Banner.svg";
+import image1 from "../assets/SignInImg/image1.svg";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import "./SignIn.css";
+const SignIn = () => {
+  return (
+    <div className="backgroud-white inter-b">
+      <img src={image1} alt="" />
+      <div className="Box-1">
+        Welcome to <br />
+        Systempackage
+        <form action="">
+          <div className="input-div">
+            <input type="text" placeholder="HelloUser" />
+          </div>
+          <div className="input-div">
+            <input type="password" />
+            <div className="eye-icon-background">
+              <VisibilityIcon />
+            </div>
+            <div className="eye-icon-background">
+              <VisibilityOffIcon />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
